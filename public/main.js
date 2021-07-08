957debd6c2fb538a7f5c3741778d2eeb67c940ee
AZUMA, Riona <219c0012@s.obirin.ac.jp>
2021-07-08 18:22:17 +0900

    送信者がわかるように最初に名前を入れる

--- public/main.js
+++ public/main.js
@@ -3,6 +3,26 @@ const form = document.getElementById("form");
 const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
+const nameForm = document.getElementById("nameForm");
+const userName = document.getElementById("userName");
+
+let username='';
+nameForm.addEventListener('submit', function(event){
+  if(userName.value!==''){
+    username = userName.value;
+    nameForm.style.display ="none";
+    form.style.display ="flex";
+
+    socketio.emit('signin');
+
+    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
+    socketio.emit('message', msg);
+  }
+
+  event.preventDefault();
+})
+
+
 form.addEventListener('submit', function(event){
   if(input.value!==''){
     socketio.emit('message', input.value);
